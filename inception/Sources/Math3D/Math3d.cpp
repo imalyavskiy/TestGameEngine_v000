d307062f2c1b8d4c23e3ad7bf6ea60543bf0568c
Ilya Malyavskiy <ilya.malyavskiy@gmail.com>
2022-08-10 23:33:46 +0700

    smooth rotation and less heavy rotation calculation

--- inception/Sources/Math3D/Math3d.cpp
+++ inception/Sources/Math3D/Math3d.cpp
@@ -52,62 +52,72 @@ namespace Math3D {
 
 	Matrix4f Transform::Create(Position position)
 	{
-		return Matrix4f(
-			{ 1.f, 0.f, 0.f, position.x },
-			{ 0.f, 1.f, 0.f, position.y },
-			{ 0.f, 0.f, 1.f, position.z },
-			{ 0.f, 0.f, 0.f, 1.f }
-		);
+		return {
+			1.f, 0.f, 0.f, position.x,
+			0.f, 1.f, 0.f, position.y,
+			0.f, 0.f, 1.f, position.z,
+			0.f, 0.f, 0.f,    1.f
+		};
 	}
 
 	Matrix4f Transform::Create(Rotation rotation)
 	{
-		float sroll = std::sinf(rotation.x);
-		float croll = std::cosf(rotation.x);
-		
-		float spitch = std::sinf(rotation.y);
-		float cpitch = std::cosf(rotation.y);
+		const float sr = std::sinf(rotation.x); // sin(roll)
+		const float cr = std::cosf(rotation.x); // cos(roll)
+		const float sp = std::sinf(rotation.y); // sin(pitch)
+		const float cp = std::cosf(rotation.y); // cos(pitch)
+		const float sy = std::sinf(rotation.z); // sin(yaw)
+		const float cy = std::cosf(rotation.z); // cos(yaw)
 
-		float syaw = std::sinf(rotation.z);
-		float cyaw = std::cosf(rotation.z);
-
-		//TODO: optimize - there are a lot of multiplications to ZERO. If to calculate the resulting matrix 
-		// and set the precalculated sin and cos values there will be much less operations;
+	/*
 		const Matrix4f rx
 		{
 			1.0f,  0.0f,   0.0f, 0.0f,
-			0.0f, croll, -sroll, 0.0f,
-			0.0f, sroll,  croll, 0.0f,
+			0.0f,   cr ,   -sr , 0.0f,
+			0.0f,   sr ,    cr , 0.0f,
 			0.0f,  0.0f,   0.0f, 1.0f,
 		};
-
+	
 		const Matrix4f ry
 		{
-			cpitch, 0.0f, -spitch, 0.0f,
-			  0.0f, 1.0f,    0.0f, 0.0f,
-			spitch, 0.0f,  cpitch, 0.0f,
-			  0.0f, 0.0f,    0.0f, 1.0f,
+			 cp , 0.0f, -sp , 0.0f,
+			0.0f, 1.0f, 0.0f, 0.0f,
+			 sp , 0.0f,  cp , 0.0f,
+			0.0f, 0.0f, 0.0f, 1.0f,
 		};
-
+	
 		const Matrix4f rz
 		{
-			cyaw, -syaw, 0.0f, 0.0f,
-			syaw,  cyaw, 0.0f, 0.0f,
+			 cy ,  -sy , 0.0f, 0.0f,
+			 sy ,   cy , 0.0f, 0.0f,
 			0.0f,  0.0f, 1.0f, 0.0f,
 			0.0f,  0.0f, 0.0f, 1.0f,
 		};
-
+	
+		// This variant takes 128 multiplciation and 96 addition operations.
 		return rz * ry * rx;
+	*/
+
+		// This is the analitically deduced matrix after 2 multiplications.
+		// This variant has 16 multiplication and 4 addition operations. Such 
+		// advantage reached because of all multiplications by ZERO were 
+		// precalculated.
+		return {
+			cy*cp, (-sy)*cr+cy*(-sp)*(-sr), (-sy)*sr+cy*(-sp)*cr, 0,
+			sy*cp,   cy*cr+sy*(-sp)*(-sr) ,   cy*sr+sy*(-sp)*cr , 0,
+			  sp ,         cp*(-sr)       ,         cp*cr       , 0,
+			   0 ,           0            ,           0         , 1,
+		};
 	}
 
 	Matrix4f Transform::Create(Scale scale)
 	{
-		return Matrix4f(
-			{ scale.x,     0.f,     0.f,     0.f },
-			{     0.f, scale.y,     0.f,     0.f },
-			{     0.f,     0.f, scale.z,     0.f },
-			{     0.f,     0.f,     0.f,     1.f }
-		);
+		return {
+			scale.x,   0.f  ,   0.f  ,  0.f,
+			  0.f  , scale.y,   0.f  ,  0.f,
+			  0.f  ,   0.f  , scale.z,  0.f,
+			  0.f  ,   0.f  ,   0.f  ,  1.f
+		};
 	}
 }
 
